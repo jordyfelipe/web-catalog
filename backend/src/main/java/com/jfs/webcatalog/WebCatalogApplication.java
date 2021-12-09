eb2c89c5975e6a9fd0f16dc1609158b83171431a
Jordy Felipe da Silva <jordyf.silva@hotmail.com>
2021-12-09 20:00:13 -0200

    Backend project created

---
+++ backend/src/main/java/com/jfs/webcatalog/WebCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.jfs.webcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jfs/webcatalog/WebCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfs.webcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
