33a80cfbb5eb6ec900e42f00e00bc2407a55dc92
Jordy Felipe da Silva <jordyf.silva@hotmail.com>
2021-12-12 20:31:50 -0200

    DTO

--- backend/src/main/java/com/jfs/webcatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/jfs/webcatalog/resources/CategoryResource.java
@@ -8,19 +8,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jfs.webcatalog.dto.CategoryDTO;
 import com.jfs.webcatalog.entities.Category;
 import com.jfs.webcatalog.services.CategoryService;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
-	
+
 	@Autowired
 	private CategoryService service;
-	
+
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll(){
-		List<Category> list = service.findAll();		
+	public ResponseEntity<List<CategoryDTO>> findAll() {
+		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
--- backend/src/main/java/com/jfs/webcatalog/services/CategoryService.java
+++ backend/src/main/java/com/jfs/webcatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.jfs.webcatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.jfs.webcatalog.dto.CategoryDTO;
 import com.jfs.webcatalog.entities.Category;
 import com.jfs.webcatalog.repositories.CategoryRepository;
 
@@ -14,16 +16,30 @@ do controle de injeção de dependências do Spring*/
 
 @Service
 public class CategoryService {
-	
-	/*Anotação para injetar dependência válida 
-	 * do Repository*/
+
+	/*
+	 * Anotação para injetar dependência válida do Repository
+	 */
 	@Autowired
 	private CategoryRepository repository;
-	
-	/*Transação de apenas leitura, 
-	 * não é necessário locking no banco.*/
+
+	/*
+	 * Transação de apenas leitura, não é necessário locking no banco.
+	 */
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		
+		/* Forma alternativa
+		List<CategoryDTO> listDto = new ArrayList<>();
+		for (Category cat : list) {
+			listDto.add(new CategoryDTO(cat));
+		}
+		return listDto;
+		*/
+		
+		//Recurso Lambda Java:
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		
 	}
 }
